5b0581c3ed68f6866507493bd9e8308c80440b39
Amirbek Pulatov <78723532+ReDJoHn2000@users.noreply.github.com>
2025-02-18 18:58:36 +0500

    file: main source

---
+++ src/main.c
@@ -0,0 +1,8 @@
+                                                                        #include <stdio.h>
+
+                                                                        int main(void){
+                                                                            const char *myFullName = "Amirbek Pulatov";
+                                                                            char *role = "Full-stack Developer";
+
+                                                                            return 0;
+                                                                        }
