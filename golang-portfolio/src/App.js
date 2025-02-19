a1359d0e23ef711fe4ccfdc2f97a71fd090dfac9
Amirbek Pulatov <78723532+ReDJoHn2000@users.noreply.github.com>
2025-02-19 09:34:37 +0500

    feature: expanding the page into three sections

--- golang-portfolio/src/App.js
+++ golang-portfolio/src/App.js
@@ -1,15 +1,21 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
-  return(
+  return (
     <Router>
       <div className="min-h-screen bg-gray-900 text-white">
         <nav className="p-4 bg-gray-800 flex justify-between">
           <h1 className="text-xl font-bold">Golang Dev</h1>
           <div className="space-x-4">
-            <Link to="/" className="hover:underline">About</Link>
-            <Link to="/projects" className="hover:underline">Projects</Link>
-            <Link to="/contact" className="hover:underline">Contact</Link>
+            <Link to="/" className="hover:underline">
+              About
+            </Link>
+            <Link to="/projects" className="hover:underline">
+              Projects
+            </Link>
+            <Link to="/contact" className="hover:underline">
+              Contact
+            </Link>
           </div>
         </nav>
 
@@ -22,3 +28,11 @@ function App() {
     </Router>
   );
 }
+
+function About() {}
+
+function Projects() {}
+
+function Contact() {}
+
+export default App;
